370cbe7d8337d958f7b9d09e81d6eda07714346d
陈传东 <>
2017-01-04 21:36:52 +0800

    test

--- app/src/main/java/demo/example/com/myradarapplication/MainActivity.java
+++ app/src/main/java/demo/example/com/myradarapplication/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+//注释
         initView( );
 
         initData();
